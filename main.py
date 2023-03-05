1a58ffd8a5d0f032d4aa0f445032366bbaf4e201
szymonhel <szhelmecki@gmail.com>
2023-03-05 22:41:19 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+
+from tkinter import Tk, Canvas, PhotoImage, Label, Button
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+title_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+photo = PhotoImage(file='./tomato.png')
+canvas.create_image(103, 109, image=photo)
+canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="start")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="reset")
+reset_button.grid(column=2, row=2)
+
+check_mark = Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20))
+check_mark.grid(column=1, row=3)
+window.mainloop()
